c72c027dc66f4a3a71e1d0b464b98ac4f5010831
aetherpyromancy <imir.norlin.te12@sodetalje.ntig.se>
2015-09-11 12:33:33 +0200

    Written links to websites in index. Cleared some errors in helloagain.

---
+++ forex.py
@@ -0,0 +1,14 @@
+import requets
+
+
+def convert(fromcurrency,tocurrency,amount):
+
+    uri = "http://api.fixer.io/latest?base=%s" % fromcurrency
+
+    response=requests.get(uri)
+
+    # todo check for 200
+
+    data=response.json()
+
+    return data['rates']
